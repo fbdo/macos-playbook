e98bbee7c4e1659d4a39909e7825b8a5d3f30f0b
Oliveira Fabio; BMW Group <Fabio.Oliveira@bmw.de>
2018-03-14 14:25:04 +0100

    Reduced tasks in the recovery script to a minimum

--- recover_mac.sh
+++ recover_mac.sh
@@ -5,10 +5,6 @@ brew tap caskroom/cask
 
 brew upgrade brew-cask
 
-curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh
-
-curl -Lks https://licdci01.bmwgroup.net:7993/users/q438028/repos/dotfiles/raw/bin/install_dotfiles.sh?at=refs%2Fheads%2Fmaster | /bin/bash
-
 brew install git
 
 git clone https://github.com/ansible-macos/macos-playbook.git ~/Workspace/macos-playbook
